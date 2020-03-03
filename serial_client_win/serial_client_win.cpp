ee4d03769a621747e49e6470e0a66ff2bd0cbe37
aty@poczta.onet.pl <aty@poczta.onet.pl>
2020-03-03 19:51:40 +0100

    kopia przed wgraniem socket_client_win

---
+++ serial_client_win/serial_client_win.cpp
@@ -0,0 +1,6 @@
+#include "serial_client_win.h"
+
+serial_client_win::serial_client_win()
+{
+
+}
---
+++ serial_client_win/serial_client_win.h
@@ -0,0 +1,11 @@
+#ifndef SERIAL_CLIENT_WIN_H
+#define SERIAL_CLIENT_WIN_H
+#include <gtkmm.h>
+
+class serial_client_win : public Gtk::Window
+{
+public:
+    serial_client_win();
+};
+
+#endif // SERIAL_CLIENT_WIN_H
